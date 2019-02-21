bd0b66fa40b4b75d667421f3a6577a5ad1fe23b8
Tatyana Zamkovaya <tatyanaz@ufl.edu>
2019-02-21 04:23:47 -0500

    removed extra comments

--- app.R
+++ app.R
@@ -25,17 +25,11 @@ ui <- dashboardPage(skin = "purple",
                                   menuItem("Alpha Diversity Analysis", tabName = "ad1", icon = icon("bar-chart-o")),
                                   conditionalPanel("input.tabs === 'ad1'",
                                                    fluidPage(
-                                                     #selectInput(inputId= "site", label="Choose body subsite:",
-                                                                 #choices= c("Saliva", "Stool", "Tongue Dorsum", "Anterior Nares", "Left Retroauricular Crease", "Left Antecubital Fossa", "Mid Vagina"), #multiple=!F),
-                                                     #)
                                                    radioButtons(inputId = "comparison_type", label="Compare alpha diversity by:",
                                                                 choices = c("SEX", "HMP_BODY_SITE", "HMP_BODY_SUBSITE", "RUN_CENTER"))
                                                    )
-                                                     #hr(),
-                                                     #radioButtons(inputId = "taxa_class", label="Choose taxonomic classification level:", list("Phylum"="Phylum","Class"="Class","Order"="Order","Family"="Family","Genus"="Genus")),
-                                                     #selectInput(inputId = "taxa_class", label="Choose taxonomic classification level:", choices=c("Phylum","Class","Order","Family","Genus")),
+                                                     
                                   ),
-                      
                                 menuItem("PCoA Analysis", tabName = "pcoa", icon = icon("fas fa-arrows-alt")),
                                 conditionalPanel("input.tabs == 'pcoa'",
                                                  fluidPage(
@@ -50,14 +44,6 @@ ui <- dashboardPage(skin = "purple",
                                 menuItem("Network Analysis", tabName = "net1", icon = icon("cog")),
                                 conditionalPanel("input.tabs == 'net1'",
                                                  fluidPage(
-                                                   # radioButtons(inputId = "site3", label = "Choose body subsite:", list("Saliva" = "Saliva",
-                                                   #                                                                      "Stool" = "Stool",
-                                                   #                                                                      "Anterior Nares" = "Anterior Nares",
-                                                   #                                                                      "Left Antecubital Fossa" = "Left Antecubital Fossa",
-                                                   #                                                                      "Mid Vagina" = "Mid Vagina",
-                                                   #                                                                      "Left Retroauricular Crease" = "Left Retroauricular Crease",
-                                                   #                                                                      "Tongue Dorsum" = "Tongue Dorsum"))
-                                                   #              ,
                                                    selectInput(inputId = "tax_rank", label="Choose taxonomic classification level:", choices = c("PHYLUM", "CLASS", "ORDER", "FAMILY", "GENUS")),
                                                   actionButton(inputId = "hub", label="View hubs", value=NULL)
                                                    )
@@ -105,22 +91,6 @@ server <- function(input, output, session){
   library(plotly)
   load("~/HMP16S/V13_HMP_phylo1.RData")
   
-  # #Relative Abundance Analysis
-  # #first, subset by body subsite using subset_samples function
-  # new_body_site_phylo <- list()
-  # #body_sites <- as.list(unique(sample_data(V13_HMP_phylo1)$HMP_BODY_SUBSITE))
-  #   for(body_site in unique(sample_data(V13_HMP_phylo1)$HMP_BODY_SUBSITE)){
-  #   print(body_site)
-  #   phylo_reduced1 <- subset_samples(V13_HMP_phylo1, HMP_BODY_SUBSITE == body_site)
-  #   print(phylo_reduced1)
-  #   #now only keep most abundant taxa by filtering out all taxa not seen more than 3 times in at least 20 % of all samples
-  #   phylo_reduced2 = filter_taxa(phylo_reduced1, function(x) sum(x > 3) > (0.2*length(x)), TRUE)
-  #   #Remove any samples that have less than 5000 total reads
-  #   phylo_reduced3 <- prune_samples(sample_sums(phylo_reduced2)>=5000, phylo_reduced2)
-  #   phylo_reduced3_percent = transform_sample_counts(phylo_reduced3, function(x) 100 * x/sum(x))
-  #   new_body_site_phylo[[body_site]] = phylo_reduced3_percent
-  # }
-  # save(new_body_site_phylo, file = "~/HMP16S/all_subsite_phylos.RData")
   #Relative Abundance Analysis
   load("~/HMP16S/all_subsite_phylos.RData")
   output$abund_plot <- renderPlot({
@@ -132,10 +102,6 @@ server <- function(input, output, session){
     ggtitle(paste(input$bsite, input$rank, "Relative Abundance", sep=" "))
   })
   
-  # V13_HMP_phylo1_allsubsites <- merge_samples(V13_HMP_phylo1, "HMP_BODY_SUBSITE") #now grouped by body subsite, so there are only 7 "samples"
-  # V13_HMP_phylo1_allsubsites2 = filter_taxa(V13_HMP_phylo1_allsubsites, function(x) sum(x > 3) > (0.2*length(x)), TRUE)
-  # V13_HMP_phylo1_allsubsites3 = transform_sample_counts(V13_HMP_phylo1_allsubsites2, function(x) 100 * x/sum(x))
-  # save(V13_HMP_phylo1_allsubsites3, file="~/HMP16S/V13_HMP_allsubsites.RData")
   load("~/HMP16S/V13_HMP_allsubsites.RData")
   V13_HMP_psmelt_3 <- psmelt(V13_HMP_phylo1_allsubsites3)
   
@@ -146,8 +112,6 @@ server <- function(input, output, session){
     geom_bar(stat="identity") + theme(axis.title.x = element_blank()) + ylab("Percentage of Sequences") + theme_classic() +
     theme(legend.text = element_text(size=rel(0.35))) + theme(axis.text.x = element_text(angle=45, hjust=1)) + guides(alpha=FALSE)
   })
-  # plot_bar(V13_HMP_phylo1_allsubsites3, x = "Sample", y = "Abundance", fill = "PHYLUM") + 
-    #geom_bar(stat="identity", aes(sample_data(V13_HMP_phylo1_allsubsites3)[rownames(sample_data(V13_HMP_phylo1_allsubsites3)) == "Stool"], aes("Sample", alpha=0, size=1, color="black")) + theme_classic()
   
   #Alpha Diversity Analysis
   V13_HMP_phylo1 %>% sample_samples(100) -> V13_HMP_phylo1_100
@@ -159,8 +123,7 @@ server <- function(input, output, session){
   
   #PCoA Analysis
   load("~/HMP16S/V13_HMP_ord.RData")
-  # output$pcoa_plot <- renderPlot({
-  #   
+    
   V13_HMP_phylo1_ord_100 <- ordinate(V13_HMP_phylo1_100, method="PCoA", distance="bray")
   ord_plot <- reactive({
     if(input$type_ord == TRUE){
@@ -186,18 +149,6 @@ server <- function(input, output, session){
     ggplotly(p)
     })
   
-  #Subsite Network Analysis
-  # subsite_graphs_list = list()
-  # for(name in names(new_body_site_phylo)){
-  #   print(name)
-  #   new_phylo_for_net <- new_body_site_phylo[[name]]
-  #   new_phylo_spiec.out=spiec.easi(new_phylo_for_net, method="mb",icov.select.params=list(rep.num=20))
-  #   new_phylo_spiec.graph=adj2igraph(new_phylo_spiec.out$refit, vertex.attr=list(name=taxa_names(new_phylo_for_net)))
-  #   keep_info = list(new_phylo_for_net, new_phylo_spiec.graph)
-  #   names(keep_info) = c("Phylo", "Graph")
-  #   subsite_graphs_list[[name]] = keep_info
-  # }
-  # save(subsite_graphs_list, file="~/HMP16S/HMP_subsite_graphs_list.RData")
   load("~/HMP16S/HMP_subsite_graphs_list.RData")
   output$net2_plot <- renderPlotly({
     new_phylo_for_net <- subsite_graphs_list[[input$net_type]]
